8c3fce31ebc985e2065946df614fbeaf20423ff0
sumit <sumitjha4552@gmail.com>
2025-06-03 10:25:47 +0530

    project done

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ import authRoutes from "./routes/auth.routes.js";
 import aiRoutes from "./routes/ai.routes.js";
 import logRoutes from "./routes/log.routes.js";
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 connect();
@@ -18,4 +20,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/ai', aiRoutes);
 app.use('/api/logs', logRoutes);
 app.get('/', (req, res) => res.send('Xeno CRM API running'));
-app.listen(5000, () => console.log('Backend running on port 5000'));
+
+app.listen(PORT, () => console.log('Backend running on port 5000'));
